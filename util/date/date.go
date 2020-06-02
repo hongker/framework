c9b84f7e6606a6be8da7179a4ccb5a80dea2fd1c
min.chen <min.chen@yingxiong.com>
2020-06-02 10:08:17 +0800

    时间

--- util/date/date.go
+++ util/date/date.go
@@ -20,22 +20,18 @@ func GetLocalTimeZone() *time.Location {
 	return time.FixedZone("CST", 8*3600) // UTC+8
 }
 
-// GetLocalDateStr 返回日期，如： 2019-12-30
-func GetLocalDateStr() string {
-	return GetLocalTime().Format(DateFormat)
-}
-
-// GetLocalTimeStr 返回时间，如:2019-12-30 22:00:00
-func GetLocalTimeStr() string {
-	return GetLocalTime().Format(TimeFormat)
-}
-
-// GetLocalTimeStamp 返回时间戳
-func GetLocalTimeStamp() int64 {
-	return GetLocalTime().Unix()
-}
-
 // GetLocalMicroTimeStampStr 返回ms
 func GetLocalMicroTimeStampStr() string {
 	return fmt.Sprintf("%.6f", float64(GetLocalTime().UnixNano())/1e9)
+}
+
+// FormatTimeInLocation 格式化时间戳
+func TimestampInLocation(timestamp int64, loc *time.Location) time.Time  {
+	return time.Unix(timestamp, 0).In(loc)
+}
+
+// ParseTimeInLocation 格式化时间
+func ParseTimeInLocation(timeStr string, loc *time.Location) time.Time  {
+	tmp, _ := time.ParseInLocation(TimeFormat, timeStr, loc)
+	return tmp
 }
